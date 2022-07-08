63216932632a140bef0461fc96ed1066d8a7a922
Pinnamma1 <108686872+Pinnamma1@users.noreply.github.com>
2022-07-08 18:46:10 +0530

    Add files via upload

--- task-2.py
+++ task-2.py
@@ -1,34 +1,28 @@
 # Statistics Formulas:
 # Mean = sum of items/ no.of items
-'''l = [1,2,3,4,5,6,7,8]
-x = sum(l)
-y = len(l)
+l = [1,2,3,4,5,6,7,8]
+x = sum(l)       # Sum = sum of items
+y = len(l)      # len = no. of items
 mean = x / y 
-print(mean)'''
+print(mean)
 
-'''# Median = middle number in the list, if even n+1 /2 , if it odd n/2, n is no.of items
+# Median =  number of items divided by 2 in the list, if even n+1 /2 , if it odd n/2, n is no.of items
 l = [1,2,3,4,5,6,7,8]
 median = (len(l)+1)/ 2
-print(median)'''
-
-'''from collections import Counter
+print(median)
+# Mode = most frequent number in data
+from collections import Counter
 def mode(sample):
     c = Counter(sample)
     return [k for k, v in c.items() if v==c.most_common(1)[0][1]]
-mode([4,1,2,2,3,5])''''''
+mode([4,1,2,2,3,5])
 
-'''
-'''
-sample = [4,1,2,2,3,5]
-c= Counter(sample)
-for v in c.items():
-    if v == c.most_common(1)[0][1]:'''
-
+# Variance & Standard Deviation
 list = [1,2,4,6,7,8,9]
 avg = sum(list)/len(list)
 var = sum((x-avg)**2 for x in list) / len(list)
-print(var)
+print(var)                 # var-Variance
 
-stad = var**0.5
-print(stad)
+stad = var**0.5            # Stad - Standard deviation
+print(stad)        
 
